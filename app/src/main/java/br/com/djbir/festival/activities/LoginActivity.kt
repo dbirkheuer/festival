7b00fa42bef936ce67b8c58ca58b21efcce15245
Douglas <douglas.joel@retta.com.br>
2019-05-19 16:27:36 -0300

    Criação do projeto

---
+++ app/src/main/java/br/com/djbir/festival/activities/LoginActivity.kt
@@ -0,0 +1,13 @@
+ package br.com.djbir.festival.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.djbir.festival.R
+
+ class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/br/com/djbir/festival/fragments/LogoFragment.kt
@@ -0,0 +1,17 @@
+package br.com.djbir.festival.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.djbir.festival.R
+
+class LogoFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_logo, container, false)
+    }
+}
